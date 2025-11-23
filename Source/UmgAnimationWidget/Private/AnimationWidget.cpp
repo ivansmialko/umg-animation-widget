16895b7b6bb2d951489ee4612e8634f81182b9ff
ivansmialko <ivansmyalko@gmail.com>
2025-11-24 01:21:53 +0200

    Initial commit

---
+++ Source/UmgAnimationWidget/Private/AnimationWidget.cpp
@@ -0,0 +1,1 @@
+#include "AnimationWidget.h"
---
+++ Source/UmgAnimationWidget/Public/AnimationWidget.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+#include "AnimationWidget.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class UAnimationWidget : public UUserWidget
+{
+    GENERATED_BODY()
+};
