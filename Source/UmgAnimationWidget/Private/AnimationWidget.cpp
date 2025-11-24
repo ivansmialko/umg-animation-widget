f33545c97ebf04848e82a8eb7629d3122ac916b1
ivan.smialko <ivan.smialko@ab-games.com>
2025-11-24 18:39:37 +0200

    Plugin implementation

--- Source/UmgAnimationWidget/Private/AnimationWidget.cpp
+++ Source/UmgAnimationWidget/Private/AnimationWidget.cpp
@@ -1,1 +1,146 @@
 #include "AnimationWidget.h"
+
+#include "Components/Image.h"
+
+const FName UAnimationWidget::MaterialParameterFrameRate = TEXT("FrameRate");
+const FName UAnimationWidget::MaterialParameterColumnsAmount = TEXT("NumberOfColumns");
+const FName UAnimationWidget::MaterialParameterRowsAmount = TEXT("NumberOfRows");
+const FName UAnimationWidget::MaterialParameterStaticFrameNumber = TEXT("StaticFrameNumber");
+const FName UAnimationWidget::MaterialParameterAnimationTexture = TEXT("AnimationTexture");
+const FName UAnimationWidget::MaterialParameterTime = TEXT("InputTime");
+
+void UAnimationWidget::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	InitializeMaterial();
+}
+
+void UAnimationWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
+void UAnimationWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	if (IsPaused)
+		return;
+
+	CurrentTime += InDeltaTime;
+	CurrentTime = FMath::Fmod(CurrentTime, GetAnimationDuration());
+	float NormalTime = FMath::Clamp(CurrentTime / GetAnimationDuration(), 0.f, 1.f);
+	SetTimeInput(NormalTime);
+}
+
+void UAnimationWidget::AnimationPlay()
+{
+	IsPaused = false;
+}
+
+void UAnimationWidget::AnimationPause()
+{
+	IsPaused = true;
+}
+
+void UAnimationWidget::AnimationStop()
+{
+	IsPaused = true;
+	SetFrame(StaticFrameNumber);
+}
+
+void UAnimationWidget::SetFrame(int32 InFrame)
+{
+	float FramesAmount = static_cast<float>(GetFramesAmount());
+	if (FramesAmount == 0)
+		return;
+
+	float Percent = (1 / FramesAmount) * static_cast<float>(InFrame);
+	CurrentTime = (GetAnimationDuration() * Percent);
+	SetTimeInput(Percent);
+}
+
+void UAnimationWidget::InitializeMaterial()
+{
+	if (!AnimationMaterialInstance)
+		return;
+
+	if (!AnimationTexture)
+		return;
+
+	if (!AnimImage)
+		return;
+
+	AnimationMaterialInstanceDynamic = UMaterialInstanceDynamic::Create(AnimationMaterialInstance, this);
+	if (!AnimationMaterialInstanceDynamic)
+		return;
+
+	SetFrameRate(FrameRate);
+	SetColumnsAmount(ColumnsAmount);
+	SetRowsAmount(RowsAmount);
+	SetStaticFrameNumber(StaticFrameNumber);
+	SetAnimationTexture(AnimationTexture);
+
+	FSlateBrush AnimatedBrush = AnimImage->GetBrush();
+	AnimatedBrush.SetResourceObject(AnimationMaterialInstanceDynamic);
+	AnimImage->SetBrush(AnimatedBrush);
+}
+
+void UAnimationWidget::SetTimeInput(float InTime)
+{
+	if (!AnimationMaterialInstanceDynamic)
+		return;
+
+	AnimationMaterialInstanceDynamic->SetScalarParameterValue(MaterialParameterTime, FMath::Clamp(InTime, 0.f, 1.f));
+}
+
+int32 UAnimationWidget::GetCurrentFrame() const
+{
+	if (CurrentTime == 0.f)
+		return 0;
+
+	return static_cast<int32>(FMath::FloorToInt(CurrentTime * static_cast<float>(FrameRate))) % GetFramesAmount();
+}
+
+void UAnimationWidget::SetFrameRate(int32 InFrameRate)
+{
+	if (!AnimationMaterialInstanceDynamic)
+		return;
+
+	FrameRate = InFrameRate;
+	AnimationMaterialInstanceDynamic->SetScalarParameterValue(MaterialParameterFrameRate, static_cast<float>(InFrameRate));
+}
+
+void UAnimationWidget::SetColumnsAmount(int32 InColumnsAmount)
+{
+	if (!AnimationMaterialInstanceDynamic)
+		return;
+
+	ColumnsAmount = InColumnsAmount;
+	AnimationMaterialInstanceDynamic->SetScalarParameterValue(MaterialParameterColumnsAmount, static_cast<float>(InColumnsAmount));
+}
+
+void UAnimationWidget::SetRowsAmount(int32 InRowsAmount)
+{
+	if (!AnimationMaterialInstanceDynamic)
+		return;
+
+	RowsAmount = InRowsAmount;
+	AnimationMaterialInstanceDynamic->SetScalarParameterValue(MaterialParameterRowsAmount, static_cast<float>(InRowsAmount));
+}
+
+void UAnimationWidget::SetStaticFrameNumber(int32 InStaticFrameNumber)
+{
+	StaticFrameNumber = InStaticFrameNumber;
+}
+
+void UAnimationWidget::SetAnimationTexture(UTexture2D* InAnimationTexture)
+{
+	if (!InAnimationTexture)
+		return;
+
+	if (!AnimationMaterialInstanceDynamic)
+		return;
+
+	AnimationTexture = InAnimationTexture;
+	AnimationMaterialInstanceDynamic->SetTextureParameterValue(MaterialParameterAnimationTexture, InAnimationTexture);
+}
--- Source/UmgAnimationWidget/Public/AnimationWidget.h
+++ Source/UmgAnimationWidget/Public/AnimationWidget.h
@@ -4,33 +4,146 @@
 
 #include "AnimationWidget.generated.h"
 
+class UImage;
 class UTexture2D;
+class UMaterialInstance;
 class UMaterialInstanceDynamic;
 
 UCLASS(Blueprintable, BlueprintType)
 class UAnimationWidget : public UUserWidget
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
-//private fields
+	static const FName MaterialParameterFrameRate;
+	static const FName MaterialParameterColumnsAmount;
+	static const FName MaterialParameterRowsAmount;
+	static const FName MaterialParameterStaticFrameNumber;
+	static const FName MaterialParameterAnimationTexture;
+	static const FName MaterialParameterTime;
+
+	//private fields
 private:
-	/* Set your animation spreadsheet */
-	UPROPERTY(EditAnywhere, Category="Animation")
+
+	UPROPERTY(EditDefaultsOnly, Category = "Animation")
+	UMaterialInstance* AnimationMaterialInstance{ nullptr };
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Animation")
+	UMaterialInstanceDynamic* AnimationMaterialInstanceDynamic{ nullptr };
+
+	/* Current animation time. Mapped to 0-1 in Tick */
+	UPROPERTY(VisibleDefaultsOnly, Category = "Animation")
+	float CurrentTime{ 0.f };
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Animation")
+	bool IsPaused{ false };
+
+	/* Set your animation sprite sheet */
+	UPROPERTY(EditAnywhere, Category = "Animation")
 	UTexture2D* AnimationTexture{ nullptr };
 
 	/* Set desired animation frame rate */
 	UPROPERTY(EditAnywhere, Category = "Animation")
-	uint32 FrameRate{ 30 };
+	int32 FrameRate{ 30 };
 
 	/* Set amount of columns in your animation spreadsheet */
 	UPROPERTY(EditAnywhere, Category = "Animation")
-	uint32 ColumnsAmount{ 0 };
+	int32 ColumnsAmount{ 0 };
 
 	/* Set amount of rows in your animation spreadsheet */
 	UPROPERTY(EditAnywhere, Category = "Animation")
-	uint32 RowsAmount{ 0 };
+	int32 RowsAmount{ 0 };
 
 	/* Set static frame number (0 based) if you want to display a static frame instead of an animation */
 	UPROPERTY(EditAnywhere, Category = "Animation")
-	uint32 StaticFrameNumber{ 0 };
+	int32 StaticFrameNumber{ 0 };
+
+	/* Image in the UMG control that will display animation */
+	UPROPERTY(meta = (BindWidget))
+	UImage* AnimImage{ nullptr };
+
+//public methods
+public:
+	/* Initializes widget in editor */
+	void NativePreConstruct() override;
+
+	/* Initializes widget in runtime */
+	void NativeConstruct() override;
+
+	/* Tick is used to play, pause and stop animation */
+	void NativeTick(const FGeometry& MyGeometry, float InDeltaTime);
+
+	/* Plays or resumes animation */
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+	void AnimationPlay();
+
+	/* Pauses animation (on current frame) */
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+	void AnimationPause();
+
+	/* Stops animation (and resets frame to StaticFrameNumber) */
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+	void AnimationStop();
+
+	/* Sets animation to desired frame */
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+	void SetFrame(int32 InFrame);
+
+	/* Sets desired frame rate */
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+	void SetFrameRate(int32 InFrameRate);
+
+	/* Sets amount of columns in the sprite sheet to properly cut it */
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+	void SetColumnsAmount(int32 InColumnsAmount);
+
+	/* Sets amount of rows in the sprite sheet to properly cut it */
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+	void SetRowsAmount(int32 InRowsAmount);
+
+	/* Sets frame number, that will be selected on animation stop */
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+	void SetStaticFrameNumber(int32 InStaticFrameNumber);
+
+	/* Sets animation sprite sheet*/
+	UFUNCTION(BlueprintCallable, Category = "Animation")
+	void SetAnimationTexture(UTexture2D* InAnimationTexture);
+
+//private methods
+private:
+	/* Initializes MaterialInstance, sets all the parameters */
+	void InitializeMaterial();
+
+	void SetTimeInput(float InTime);
+
+//public getters/setters
+public:
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Animation")
+	int32 GetFrameRate() const { return FrameRate; }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Animation")
+	int32 GetColumnsAmount() const { return ColumnsAmount; }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Animation")
+	int32 GetRowsAmount() const { return RowsAmount; }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Animation")
+	int32 GetStaticFrameNumber() const { return StaticFrameNumber; }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Animation")
+	int32 GetCurrentFrame() const;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Animation")
+	int32 GetFramesAmount() const { return ColumnsAmount * RowsAmount;}
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Animation")
+	float GetAnimationDuration() const { return static_cast<float>(GetFramesAmount()) / static_cast<float>(FrameRate); }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Animation")
+	float GetFrameDuration() const { return 1.f / static_cast<float>(FrameRate); }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Animation")
+	bool GetIsPaused() const { return IsPaused; }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Animation")
+	UTexture2D* GetAnimationTexture() const { return AnimationTexture; }
 };
